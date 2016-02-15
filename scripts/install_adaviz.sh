ca0ea146e7b77106369fea608760cd1442b23e51
Bao Nguyen <qbao.nguyen@gmail.com>
2016-02-15 15:49:20 +0700

    Update install_adaviz.sh

--- scripts/install_adaviz.sh
+++ scripts/install_adaviz.sh
@@ -3,4 +3,8 @@
 if [ ! -d './adaviz' ]; then
   git clone git@github.com:adatao/adaVIZ-JS.git adaviz
   (cd adaviz && npm install && npm run build:lib)
+  
+  # add adaviz to node_modules
+  mkdir -p node_modules
+  (cd node_modules && ln -s ../adaviz)
 fi
